8a09fc60a8ad148fa9ba14a535eec8d76a430a61
Kellian Barrois <kellian07@gmail.com>
2024-06-10 00:52:40 +0200

    ajout de contact

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 import Header from "./components/header";
 import Home from "./pages/home";
 import About from "./pages/about";
@@ -9,19 +10,36 @@ import Footer from "./components/footer";
 import ContactForm from "./components/contactform"; 
 import './styles/app.css';
 
+
+function AnimatedRoutes() {
+  const location = useLocation();
+
+  return (
+    <TransitionGroup>
+      <CSSTransition
+        key={location.pathname}
+        timeout={300}
+        classNames="page"
+      >
+        <Routes location={location}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/contact-form" element={<ContactForm />} /> 
+        </Routes>
+      </CSSTransition>
+    </TransitionGroup>
+  );
+}
+
 function App() {
   return (
     <Router basename="/Mon-Portfolio">
       <div className="App">
         <Header />
         <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/contact-form" element={<ContactForm />} /> 
-          </Routes>
+          <AnimatedRoutes />
         </main>
         <Footer />
       </div>
