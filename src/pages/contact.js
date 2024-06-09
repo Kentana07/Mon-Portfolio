8f1474205b9671e69c2bf19362c7aeb1556cb5b9
Kellian Barrois <kellian07@gmail.com>
2024-06-10 01:09:29 +0200

    modif contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,7 +6,7 @@ const Contact = () => (
   <div className="contact">
     <h1>Contactez-moi</h1>
     <p>Email :   Kellian07@gmail.com </p>
-    <Link to="/contact-form">M'envoyer un message</Link>
+    <h2><Link to="/contact-form">M'envoyer un message</Link></h2>
   </div>
 );
 
